a7647cf6ccbc14fabb01426065d0daa575637f82
alexrochlitz <aerochlitz@gmail.com>
2019-02-13 17:25:50 -0500

    Setup sensor grid on chair and make alpha change colour based on new value

--- src/components/Chair.vue
+++ src/components/Chair.vue
@@ -1,69 +1,115 @@
 <template>
-    <div class="chair-box">
-        <img src="public/chair.jpg" alt="Chair">
-        <table class="chair-sensors">
-            <tr>
-                <td v-bind:style="getColor()">{{ data.bTopL }}</td>
-                <td>o</td>
-                <td>o</td>
-            </tr>
-            <tr>
-                <td> </td>
-                <td>o</td>
-                <td>o</td>
-            </tr>
-            <tr>
-                <td>o</td>
-                <td>o</td>
-                <td> </td>
-            </tr>
-            <tr>
-                <td>o</td>
-                <td>o</td>
-                <td> </td>
-            </tr>
-        </table>
+  <div class="chair-box">
+    <img class="chair-pic" src="public/chair_crop.jpg" alt="Chair">
+    <div class="sensor-grid">
+      <div class="row">
+        <div class="col">o</div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col" v-bind:style="getColor(sensorData.bTopL)"></div>
+        <div class="col"></div>
+        <div class="col" v-bind:style="getColor(sensorData.bTopR)"></div>
+        <div class="col"></div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col" v-bind:style="getColor(sensorData.bBtmL)"></div>
+        <div class="col"></div>
+        <div class="col" v-bind:style="getColor(sensorData.bBtmR)"></div>
+        <div class="col"></div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+        <div class="col"></div>
+        <div class="col" v-bind:style="getColor(sensorData.sTopL)"></div>
+        <div class="col"></div>
+        <div class="col" v-bind:style="getColor(sensorData.sTopR)"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+        <div class="col" v-bind:style="getColor(sensorData.sBtmL)"></div>
+        <div class="col"></div>
+        <div class="col" v-bind:style="getColor(sensorData.sBtmR)"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+      </div>
+      <div class="row">
+        <div class="col">o</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import { SensorData } from '../services/websocket';
+import Vue from "vue";
+import Component from "vue-class-component";
+import { SensorData } from "../services/websocket";
 
 @Component({})
+/* b: back, s: seat */
 export default class Chair extends Vue {
-    private data: SensorData = {
-        bTopL: 0,
-        bTopR: 0,
-        bBtmL: 0,
-        bBtmR: 0,
-        sTopL: 0,
-        sTopR: 0,
-        sBtmL: 0,
-        sBtmR: 0
-    };
+  private sensorData: SensorData = {
+    bTopL: 10,
+    bTopR: 10,
+    bBtmL: 10,
+    bBtmR: 10,
+    sTopL: 10,
+    sTopR: 10,
+    sBtmL: 10,
+    sBtmR: 10
+  };
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        setTimeout(() => {
-            this.data.bTopL = 100;
-        }, 3000);
-    }
+    setTimeout(() => {
+      this.sensorData.bTopL = 80;
+      this.sensorData.bTopR = 60;
+      this.sensorData.bBtmL = 70;
+      this.sensorData.bBtmR = 50;
+      this.sensorData.sTopL = 100;
+      this.sensorData.sTopR = 80;
+      this.sensorData.sBtmL = 90;
+      this.sensorData.sBtmR = 50;
+    }, 3000);
+  }
 
-    private getColor() {
-        return `background: rgb(${Math.floor(255*this.data.bTopL/100)}, 0, 0)`;
-    }
+  private getColor(sensorVal: number) {
+    return `background: rgba(114, 0, 255, ${sensorVal / 100})`;
+  }
+
 }
 </script>
 
 <style>
 
-table.chair-sensors {
-    position: absolute;
-    top: 300px;
-    left: 500px;
+.sensor-grid {
+  position: absolute;
+  top: 0px;
+  left: 0px;
 }
 
 td {
@@ -73,8 +119,7 @@ td {
 }
 
 .chair-box {
-    position: relative;
+  position: relative;
 }
-
 </style>
 
